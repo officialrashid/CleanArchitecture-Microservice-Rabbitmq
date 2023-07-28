863504e8c0baf82c88e53837ec2f4e09f967e149
rashid <rashid@rashids-MacBook-Air.local>
2023-07-28 18:36:36 +0530

    dockerization completed

--- order-service/src/rabbitmqConsume/consumer.js
+++ order-service/src/rabbitmqConsume/consumer.js
@@ -2,40 +2,51 @@ import amqp from 'amqplib';
 import createOrder from '../application/usecase/order/createOrder.js';
 import orderRepositoriInf from '../application/repositories/order/orderRepositoriInf.js';
 import orderRepositoryImp from '../framework/database/mongodb/repositories/order/orderRepositoryImp.js';
-var channel,connection;
+
 const orderDbRepository = orderRepositoriInf(orderRepositoryImp());
 
 const connect = async () => {
-try {
-const amqpServer = 'amqp://rabbitmq:5672'; // Update with RabbitMQ container name and port
- connection = await amqp.connect(amqpServer);
- channel = await connection.createChannel();
-await channel.assertQueue('ORDER');
-console.log('Connected to RabbitMQ');
-return channel; // Return the channel object
-} catch (error) {
-console.error('Error connecting to RabbitMQ:', error);
-}
+  try {
+    const amqpServer = 'amqp://rabbitmq:5672'; // Update with RabbitMQ container name and port
+    const connection = await amqp.connect(amqpServer);
+    const channel = await connection.createChannel();
+    await channel.assertQueue('ORDER');
+    console.log('Connected to RabbitMQ');
+    return channel; // Return the channel object
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error);
+  }
 };
 
-setInterval(async () => {
-console.log('order service connected to sms');
- channel = await connect();
-console.log('working order queue');
-channel.consume('ORDER', (data) => {
-const { products, userId } = JSON.parse(data.content);
+const consumeOrderQueue = async () => {
+  try {
+    const channel = await connect();
+    console.log('Order service connected to RabbitMQ');
 
-console.log('consuming order queue');
-console.log(products, 'products consuming il ethitta');
-console.log(userId, 'consuming user nte email ethitta');
-createOrder(products, userId, orderDbRepository)
-  .then((newOrder) => {
-    console.log(newOrder, 'update cheyth order queue ethittaaaa');
-    channel.ack(data);
-    channel.sendToQueue('PRODUCT', Buffer.from(JSON.stringify({ newOrder })));
-  })
-  .catch((error) => {
-    console.log('error creating order', error);
-  });
+    channel.consume('ORDER', async (data) => {
+      if (!data) return; // Handle the case where the message is empty or invalid
+      const { products, userId } = JSON.parse(data.content);
+
+      console.log('Consuming order queue');
+      console.log(products, 'Products received from queue');
+      console.log(userId, 'User ID received from queue');
+
+      try {
+        const newOrder = await createOrder(products, userId, orderDbRepository);
+        console.log('Order created:', newOrder);
+
+        channel.ack(data); // Acknowledge the message after successful processing
+        channel.sendToQueue('PRODUCT', Buffer.from(JSON.stringify({ newOrder }))); // Publish message to another queue
+      } catch (error) {
+        console.log('Error creating order:', error);
+        // Handle the error gracefully or take appropriate actions
+      }
+    });
+  } catch (error) {
+    console.error('Error consuming order queue:', error);
+  }
+};
+
+consumeOrderQueue().catch((error) => {
+  console.error('Error in consumeOrderQueue:', error);
 });
-}, 10000);
